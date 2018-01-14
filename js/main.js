d57c86fc302cb45d324af55adfe92c2fe7c6fbb8
Obramenko Alexandr <obram22@gmail.com>
2018-01-14 16:57:17 +0200

    14.01.2018 main page

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
-$(document).ready(function(){
-	gumburgerButtonAnimate();
+$(document).ready(function() {
+    gumburgerButtonAnimate();
+
+
+    scrollDown();
+
+
 });
 
 
 
 function gumburgerButtonAnimate() {
-	$('#nav-icon3').click(function(){
-		$(this).toggleClass('open');
-		$('.header-nav').toggleClass('active');
-	});
-	$('#nav-icon2').click(function(){
-		$(this).toggleClass('open');
-		$('.footer-nav').toggleClass('active');
-	});
+    $('#nav-icon3').click(function() {
+        $(this).toggleClass('open');
+        $('.header-nav').toggleClass('active');
+    });
+    $('#nav-icon2').click(function() {
+        $(this).toggleClass('open');
+        $('.footer-nav').toggleClass('active');
+    });
+}
+
+function scrollDown() {
+    $('.js-scroll').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 300, 'linear');
+    });
 }
